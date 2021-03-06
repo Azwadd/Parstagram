09cab241e1b4349c0addb0b9c340282f22094e28
azwad <azwad_shameem@outlook.com>
2021-03-05 23:49:39 -0500

    init commit

---
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.parstagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
