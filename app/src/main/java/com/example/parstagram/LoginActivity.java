27fd8a853caa9b0c940519de175c13ea15e6c4aa
azwad <azwad_shameem@outlook.com>
2021-03-11 19:33:48 -0500

    Part 1 Completed

--- app/src/main/java/com/example/parstagram/LoginActivity.java
+++ app/src/main/java/com/example/parstagram/LoginActivity.java
@@ -15,23 +15,28 @@ import android.widget.Toast;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
-
     private EditText etUsername;
     private EditText etPassword;
     private Button logIn;
+    private Button signIn;
+    private Button signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        ConstraintLayout constraintLayout = findViewById(R.id.loginMenu);
 
+        if (ParseUser.getCurrentUser() != null) {
+            goMainActivity();
+        }
         // sets up some fading colors to brighten up dull login page //
+        ConstraintLayout constraintLayout = findViewById(R.id.loginMenu);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
         animationDrawable.setEnterFadeDuration(4000);
         animationDrawable.setExitFadeDuration(7000);
@@ -40,20 +45,69 @@ public class LoginActivity extends AppCompatActivity {
         // defines all items on activity //
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
+        signIn = findViewById(R.id.signIn);
+        signIn.setText("signIn");
+        signUp = findViewById(R.id.signUp);
+        signUp.setText("signUp");
         logIn = findViewById(R.id.logIn);
         logIn.setText("login");
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logIn.setText("login");
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logIn.setText("signUp");
+                if (ParseUser.getCurrentUser() != null) {
+                    ParseUser.getCurrentUser().logOut();
+                }
+            }
+        });
 
         // Once clicked we will verify the user's inputs //
         logIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i(TAG, "onClick logIn");
-                String username = etUsername.getText().toString();
-                String password = etPassword.getText().toString();
-                loginUser(username, password);
+                if (logIn.getText().equals("login")) {
+                    Log.i(TAG, "onClick logIn");
+                    String username = etUsername.getText().toString();
+                    String password = etPassword.getText().toString();
+                    loginUser(username, password);
+                } else if (logIn.getText().equals("signUp")) {
+                    Log.i(TAG, "onClick signUp");
+                    String username = etUsername.getText().toString();
+                    String password = etPassword.getText().toString();
+                    signUpUser(username, password);
+                }
             }
         });
 
+    }
+
+    private void signUpUser(String username, String password) {
+        Log.i(TAG, "Attempting to signup user");
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username);
+        user.setPassword(password);
+        // Set custom properties
+        // Invoke signUpInBackground
+        user.signUpInBackground(new SignUpCallback() {
+            public void done(ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with signup", e);
+                    Toast.makeText(getApplicationContext(), "Error with loginaz", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    goMainActivity();
+                }
+            }
+        });
     }
 
     /** Check user credentials and displays data */
@@ -74,5 +128,6 @@ public class LoginActivity extends AppCompatActivity {
     private void goMainActivity() {
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+        finish();
     }
 }
